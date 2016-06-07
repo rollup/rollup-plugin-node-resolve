bf90417c2122f450094a2c7c3a119f9c4c873096
Rich Harris <richard.a.harris@gmail.com>
2016-06-07 12:10:12 -0400

    use config.targets, switch from babel to buble

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,11 @@
-import babel from 'rollup-plugin-babel';
+import buble from 'rollup-plugin-buble';
 
 export default {
 	entry: 'src/index.js',
-	plugins: [ babel() ],
-	external: [ 'path', 'fs', 'builtin-modules' ]
+	plugins: [ buble() ],
+	external: [ 'path', 'fs', 'builtin-modules' ],
+	targets: [
+		{ dest: 'dist/rollup-plugin-node-resolve.cjs.js', format: 'cjs' },
+		{ dest: 'dist/rollup-plugin-node-resolve.es6.js', format: 'es6' }
+	]
 };
